a5c8da7ed57fd14d9f40a4494c69004efbf536a1
Damian Raczek <draczekprojekt@gmail.com>
2023-11-11 22:04:19 +0100

    Entity Base Class & Responsvie Design Sample, Breakpoints

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -3,6 +3,7 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,37 +12,52 @@ namespace Projekt.Models.Entities
 {
     #nullable enable
     [Table("users")]
-    public class User
+    public class User : ValidatableEntity
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string FirstName { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string LastName { get; set; }
-        [Unique, MaxLength(255)]
+
+        [Unique, SQLite.MaxLength(255)]
         public string Email { get; set; }
-        [MaxLength(100)]
+
+        [SQLite.MaxLength(100)]
         public string PasswordHash { get; set; }
+
         public DateTime? BirthDate { get; set; }
-        [MaxLength(9)]
+
+        [SQLite.MaxLength(9)]
         public string? PhoneNumber { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string? City { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string? PostalName { get; set; }
-        [MaxLength(6)]
+
+        [SQLite.MaxLength(6)]
         public string? PostalCode { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string? Street { get; set; }
-        [MaxLength(5)]
+
+        [SQLite.MaxLength(5)]
         public string? StreetNumber { get; set; }
-        [MaxLength(5)]
+
+        [SQLite.MaxLength(5)]
         public string? ApartmentNumber { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string? Country { get; set; }
-        [MaxLength(255)]
+
+        [SQLite.MaxLength(255)]
         public string ProfilePictureSource { get; set; }
+
         public Role Role { get; set; } = Role.USER;
     }
 }
---
+++ Models/Entities/ValidatableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Models.Entities
+{
+    public class ValidatableEntity
+    {
+        public List<ValidationResult> validate()
+        {
+            var context = new ValidationContext(this, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(this, context, results, true);
+            return results;
+        }
+    }
+}
