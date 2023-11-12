2ec54950ffda481fb4c44204741c29925e7c044d
Damian Raczek <draczekprojekt@gmail.com>
2023-11-12 01:28:02 +0100

    Landing Page idea

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -5,16 +5,6 @@ namespace Projekt.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
-        [RelayCommand]
-        void GoToLoginPage()
-        {
-            Shell.Current.DisplayAlert("Go to login page", "Not implemented yet.", "OK");
-        }
 
-        [RelayCommand]
-        void GoToRegisterPage()
-        {
-            Shell.Current.DisplayAlert("Go to register page", "Not implemented yet.", "OK");
-        }
     }
 }
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -8,6 +8,22 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
+		
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		this.TranslateTo(0, this.TranslationY - loginFrame.Height, 500, Easing.CubicIn);
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        this.TranslateTo(0, 0, 500, Easing.CubicIn);
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+        this.TranslateTo(0, this.TranslationY + registerFrame.Height, 500, Easing.CubicIn);
+    }
 }
 
