019b292c515b6a9359d96d9f6ab0a28cee08760d
aruk3101 <draczekprojekt@gmail.com>
2023-12-17 20:17:54 +0100

    Applied advertisement RefreshView android bug fixed

--- Views/AppliedAdvertisementsPage.xaml.cs
+++ Views/AppliedAdvertisementsPage.xaml.cs
@@ -5,18 +5,25 @@ namespace Projekt.Views;
 
 public partial class AppliedAdvertisementsPage : ContentPage
 {
-	private AdvertisementsViewModel advertisementsViewModel;
-	public AppliedAdvertisementsPage(AppliedAdvertisementsViewModel vm,
-		AdvertisementsViewModel advertisementsViewModel)
-	{
-		InitializeComponent();
-		this.BindingContext = vm;
-		this.advertisementsViewModel = advertisementsViewModel;
+    private AdvertisementsViewModel advertisementsViewModel;
+    private AppliedAdvertisementsViewModel vm;
+    public AppliedAdvertisementsPage(AppliedAdvertisementsViewModel vm,
+        AdvertisementsViewModel advertisementsViewModel)
+    {
+        InitializeComponent();
+        this.BindingContext = this.vm = vm;
+        this.advertisementsViewModel = advertisementsViewModel;
 
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await vm.UpdateAdvertisements();
     }
 
     private async void MenuItem_Clicked_3(object sender, EventArgs e)
     {
-		await advertisementsViewModel.Info((sender as MenuItem).CommandParameter as Advertisement);
+        await advertisementsViewModel.Info((sender as MenuItem).CommandParameter as Advertisement);
     }
 }
